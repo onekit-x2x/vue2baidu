bee18c1c4a6acecc41f229bf571f5fd6959e3eaf
hhm19990929 <65931506+hhm19990929@users.noreply.github.com>
2020-08-03 18:05:04 +0800

    canvas

---
+++ vue_baidu/pages/canvas/canvas.js
@@ -0,0 +1,33 @@
+Page({
+    data: {
+
+    },
+    onLoad: function () {
+        // 监听页面加载的生命周期函数
+    },
+    onReady: function () {
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'green';
+        ctx.fillRect(10, 10, 100, 100);
+        // 监听页面初次渲染完成的生命周期函数
+    },
+    onShow: function () {
+        // 监听页面显示的生命周期函数
+    },
+    onHide: function () {
+        // 监听页面隐藏的生命周期函数
+    },
+    onUnload: function () {
+        // 监听页面卸载的生命周期函数
+    },
+    onPullDownRefresh: function () {
+        // 监听用户下拉动作
+    },
+    onReachBottom: function () {
+        // 页面上拉触底事件的处理函数
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角转发
+    }
+});
